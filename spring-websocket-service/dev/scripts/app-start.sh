4615c96d29dc963873a017a6a693c53ccf8180dc
Kevin Leung <kkleung2@uci.edu>
2021-04-08 01:36:19 -0700

    wait for config server to start

--- spring-websocket-service/dev/scripts/app-start.sh
+++ spring-websocket-service/dev/scripts/app-start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#while ! nc -z config-server 8888; do
-#    sleep 1
-#done
+while ! nc -z config-server 8888; do
+    sleep 1
+done
 java -jar -Dspring.profiles.active="dev" -Dspring.cloud.bootstrap.location=/config/bootstrap.yml /app.jar
