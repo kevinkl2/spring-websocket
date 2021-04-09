4a631427bfdb1bc414853dc1012db5b6b26452fa
Kevin Leung <kkleung2@uci.edu>
2021-04-09 02:40:32 -0700

    testing integration with mongodb

--- spring-websocket-service/src/main/java/us/sphor/websocket/SocketTextHandler.java
+++ spring-websocket-service/src/main/java/us/sphor/websocket/SocketTextHandler.java
@@ -2,24 +2,43 @@ package us.sphor.websocket;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.json.JSONException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 import us.sphor.websocket.model.Input;
+import us.sphor.websocket.repository.ChatNodeRepository;
 import us.sphor.websocket.repository.entities.Node;
 
 @Component
+@RequiredArgsConstructor
+@Slf4j
 public class SocketTextHandler extends TextWebSocketHandler {
+
+  private final ChatNodeRepository chatNodeRepository;
 
   @Override
   public void handleTextMessage(WebSocketSession session, TextMessage message)
       throws InterruptedException, IOException, JSONException {
 
     Input input = new ObjectMapper().readValue(message.getPayload(), Input.class);
-    Node node = Node.builder().response("test123").build();
-    session.sendMessage(new TextMessage(new ObjectMapper().writeValueAsString(node)));
-    session.sendMessage(new TextMessage("Hi " + input.getUser() + " how may we help you?"));
+
+    Node queryResult;
+
+    try {
+      queryResult = chatNodeRepository.findById(input.getUser()).get();
+    } catch (RuntimeException runtimeException) {
+      log.error("saveNode ERROR: ", runtimeException.toString());
+      throw runtimeException;
+    }
+
+    if (queryResult == null) {
+      session.sendMessage(new TextMessage("NOT FOUND"));
+    }
+
+    session.sendMessage(new TextMessage(new ObjectMapper().writeValueAsString(queryResult)));
   }
 }
